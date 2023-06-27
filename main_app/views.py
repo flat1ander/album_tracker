9a92f48da78890a807946532989cda4115483471
flat1ander <thematthewlane@gmail.com>
2023-06-26 22:21:03 -0400

    All CRUD functionality included now.

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,9 @@ from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from .models import Album
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.urls import reverse
 
 # Create your views here.
 
@@ -25,5 +28,33 @@ class AlbumsList(TemplateView):
                 context["header"] = f"Searching for {name}"
         else:
                 context['albums'] = Album.objects.all()
-                context["header"] = "All Albums"
+                context["header"] = "Matt's Favorite Albums"
         return context
+
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['name', 'artist', 'image', 'info']
+    template_name = "album_create.html"
+    def get_success_url(self):
+        return reverse('album_detail', kwargs={'pk': self.object.pk})
+
+class AlbumDetail(DetailView):
+    model = Album
+    template_name = "album_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        return context
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['name', 'artist', 'image', 'info']
+    template_name = "album_update.html"
+    def get_success_url(self):
+        return reverse('album_detail', kwargs={'pk': self.object.pk})
+
+class AlbumDelete(DeleteView):
+    model = Album
+    template_name = "album_delete_confirmation.html"
+    success_url = '/albums/'
